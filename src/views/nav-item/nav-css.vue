a43307464ec478b887e8c0b3f2ef8d9aae3e40ad
zhuyanglei <zhuyanglei@xdf.cn>
2021-01-19 15:47:28 +0800

    feat:圣杯布局+双飞翼布局

--- src/views/nav-item/nav-css.vue
+++ src/views/nav-item/nav-css.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
+  <div class="nav-container">
     <h6>圣杯布局</h6>
-    <div class="main clearfix"> main
-      <div class="center">center</div>
+    <div class="container clearfix">
+      <div class="middle">middle</div>
       <div class="left">left</div>
       <div class="right">right</div>
     </div>
     <hr>
     <h6>双飞翼布局</h6>
+    <div class="container02 clearfix">
+      <div class="middle02">middle</div>
+    </div>
+    <div class="left02">left</div>
+    <div class="right02">right</div>
     <hr>
   </div>
 </template>
@@ -29,37 +34,60 @@ h6 {
   line-height: 40px;
   text-align: center;
 }
-.main {
-  position: relative;
-  height: 500px;
+.container {
   padding: 0 200px;
-  background: lightgray;
+  position: relative;
 }
-.center,
-.left,
-.right {
+.middle {
+  background-color: lightpink;
+  height: 300px;
+  width: 100%;
   float: left;
 }
-.center {
-  width: 100%;
-  min-height: 200px;
-  background: lightcoral;
-}
 .left {
-  width: 200px;
-  min-height: 200px;
-  background: lightgoldenrodyellow;
-
-  margin-left: -100%;
-  position: absolute;
-  left: -200px;
+    background-color: red;
+    height: 300px;
+    width: 200px;
+    float: left;
+    margin-left: -100%;
+    position: relative;
+    left: -200px;
 }
+
 .right {
-  width: 200px;
-  min-height: 200px;
-  background:limegreen
+    background-color: aqua;
+    height: 300px;
+    width: 200px;
+    float: left;
+    margin-left: -200px;
+    position: relative;
+    right: -200px;
+
 }
+// 双飞翼布局
+.container02 {
+  width: 100%;
+  height: 300px;
+  float: left;
+  background-color: lightpink;
 
-
+}
+.middle02 {
+  margin: 0 200px;
+}
+.left02 {
+  width: 200px;
+  height: 300px;
+  margin-left: -100%;
+  background-color: red;
+  float: left;
+}
+.right02 {
+  width: 200px; 
+  height: 300px;
+  margin-left: -200px;
+  background-color: aqua;
+  float: left;
+}
 
 </style>
