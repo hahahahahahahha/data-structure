eeb2c1391d61a24726d3690775c97697d4d3d0fd
zhuyanglei <yanglei.zhu@mljr.com>
2021-08-23 20:16:27 +0800

    tree

--- src/views/class/common-question.js
+++ src/views/class/common-question.js
@@ -2,28 +2,98 @@
  * @Author       : zhuyanglei@xdf.cn
  * @Date         : 2021-01-20 22:52:07
  * @LastEditors  : zhuyanglei@xdf.cn
- * @LastEditTime : 2021-01-20 23:11:44
+ * @LastEditTime : 2021-08-17 22:47:53
  * @Description  : 描述信息
  */
 
+// const { delete } = require("vue/types/umd")
+
 //  递归深克隆
- export function deepCopy(obj) {
-    //  过滤特殊情况
-    if (obj === null) return null
-    if (typeof obj !== 'object') return obj
-    if (obj instanceof RegExp) {
-        return new RegExp(obj)
+//  export function deepCopy(obj) {
+//     //  过滤特殊情况
+//     if (obj === null) return null
+//     if (typeof obj !== 'object') return obj
+//     if (obj instanceof RegExp) {
+//         return new RegExp(obj)
+//     }
+//     if (obj instanceof Date) {
+//         return new Date(obj)
+//     }
+
+//     // 不直接创建对象的目的 克隆的结果和之前保持相同的所属类
+//     let newObj = new obj.constructor
+//     for (const key in obj) {
+//         if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//             newObj[key] = deepCopy(obj[key])
+//         }
+//     }
+//     return newObj
+//  }
+//  
+const list = [
+    {
+        id:1,
+        parentId: null,
+        name:"水果"
+    },
+    {
+        id: 2,
+        parentId: 1,
+        name: "水果-A"
+    },
+    {
+        id: 3,
+        parentId: 1,
+        name: "水果-B"
+    },
+    {
+        id: 4,
+        parentId: 2,
+        name: "水果-A-A1"
     }
-    if (obj instanceof Date) {
-        return new Date(obj)
+]
+// 递归方法
+const tree = (list)=>{
+
+    const root = list.find(item=>item.parentId === null)
+    const subTree = (root,list)=>{
+        return list.map(item=> item.parentId === root.id)
     }
 
-    // 不直接创建对象的目的 克隆的结果和之前保持相同的所属类
-    let newObj = new obj.constructor
-    for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            newObj[key] = deepCopy(obj[key])
-        }
+    const buildTree = (root, sub)=>{
+
     }
-    return newObj
- }
+}
+
+// 对象方法
+const toTreeData = (list)=>{
+    let result = []
+    // 1、判断传入的参数是否正确
+    if (!Array.isArray(list)) {
+        return result
+    }
+    // 删除元素的children属性
+    list.forEach(item=> {
+        // delete item.children
+    })
+
+    // 2、构建对象遍历存储子元素
+    let map = {}
+    list.forEach(item=>{
+        map[item.id] = item
+    })
+    // 3、遍历数组，找出对象中的子元素构建树形结构
+    list.forEach(item=> {
+        let parent = map[item.parentId]
+        if (parent) {
+            // 子节点
+            (parent.children||(parent.children=[])).push(item)
+
+        }else{
+            // 父节点
+            result.push(item)
+        }
+    })
+   return result
+}
+console.info(JSON.stringify(toTreeData(list)) );
